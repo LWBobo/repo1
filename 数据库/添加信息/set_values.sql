e0812a5001af2ca550496ee629157e3d5db5b5c9
qingyishi <1697668797@qq.com>
2019-10-14 17:18:56 +0800

    删除全部

--- 数据库/添加信息/set_values.sql
+++
@@ -1,5 +0,0 @@
-UPDATE lineitem a ,part b SET a.extendedprice=a.linenumber*b.retailprice where a.partkey=b.partkey;
-
-UPDATE lineitem a ,part b SET a.quantity=a.linenumber*b.retailprice*a.discount*(1+a.tax) where a.partkey=b.partkey;
-
-UPDATE orders a , lineitem b Set a.totalprice = b.quantity where a.orderkey = b.orderkey;
